37f15907bf27c9019faac8502e25bb0946fa2950
4pplet <mail@4pplet.com>
2022-04-07 09:52:51 +0200

    reverting to old style with split revision

--- config/boards/arm/cyber60_rev_b/board.cmake
+++ config/boards/arm/cyber60_rev_b/board.cmake
@@ -1,7 +1,4 @@
-#Copyright (c) 2021 swampmonster
-#
-#SPDX-License-Identifier: MIT
-#
+# SPDX-License-Identifier: MIT
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
