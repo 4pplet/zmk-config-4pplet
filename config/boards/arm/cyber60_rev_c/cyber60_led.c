f87955d6677655687e5f32380bffc493303632fc
4pplet <mail@4pplet.com>
2023-07-12 08:47:57 +0200

    Squashed commit of the following:
    commit 4609dd498a3307fab1bfeabf68d6868145c42414
    Author: 4pplet <mail@4pplet.com>
    Date:   Tue Jul 11 23:43:37 2023 +0200
    
        Rename CmakeLists.txt to CMakeLists.txt
    
    commit f3b8a7ddff129221250ac32affc47cf29784e458
    Author: 4pplet <mail@4pplet.com>
    Date:   Tue Jul 11 23:43:25 2023 +0200
    
        Rename CmakeLists.txt to CMakeLists.txt
    
    commit 7e150bf816a2c4b1685a5a5603d9d78efa486071
    Author: 4pplet <mail@4pplet.com>
    Date:   Tue Jul 11 23:43:07 2023 +0200
    
        Rename CmakeLists.txt to CMakeLists.txt
    
    commit a910ed2155ca4ccfb92b7bc39bde8a53f4d05d98
    Author: 4pplet <mail@4pplet.com>
    Date:   Tue Jul 11 22:54:41 2023 +0200
    
        restore default keymap
    
    commit 5b1643b6574f5fae05b943087c88519b129a79da
    Author: 4pplet <mail@4pplet.com>
    Date:   Tue Jul 11 08:54:22 2023 +0200
    
        Update build.yml
    
    commit 806fd249cf36cea1baad32ca710313067f479be3
    Author: 4pplet <mail@4pplet.com>
    Date:   Tue Jul 11 08:47:17 2023 +0200
    
        Update build.yml
    
    commit 737b5929329dc53cc7104d36ec71b08a556875a1
    Author: 4pplet <mail@4pplet.com>
    Date:   Tue Jul 11 08:40:40 2023 +0200
    
        Update cyber60_rev_d_defconfig
    
    commit 00ee9cda9210fccc654cbe67ee479df794ad78c8
    Author: 4pplet <mail@4pplet.com>
    Date:   Tue Jul 11 00:24:03 2023 +0200
    
        test-run
    
    commit d4a6b891125ae75b224986076a0fb32fea15967f
    Author: 4pplet <mail@4pplet.com>
    Date:   Wed Jun 28 23:25:33 2023 +0200
    
        Update cyber60_rev_d.keymap
    
    commit 623ed0920acc7ee4fd355ac23cb5364e26e4d541
    Author: 4pplet <mail@4pplet.com>
    Date:   Wed Jun 28 23:21:54 2023 +0200
    
        test

--- config/boards/arm/cyber60_rev_c/cyber60_led.c
+++ config/boards/arm/cyber60_rev_c/cyber60_led.c
@@ -4,15 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr.h>
-#include <kernel.h>
-#include <device.h>
-#include <devicetree.h>
-#include <drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
 
-#include <bluetooth/services/bas.h>
+#include <zephyr/bluetooth/services/bas.h>
 
-#include <logging/log.h>
+#include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/ble.h>
@@ -26,100 +25,71 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #if !DT_NODE_HAS_STATUS(LED_NODE_R, okay)
 #if !DT_NODE_HAS_STATUS(LED_NODE_G, okay)
 #if !DT_NODE_HAS_STATUS(LED_NODE_B, okay)
-    #error "Unsupported board: led devicetree alias is not defined"
+#error "Unsupported board: led devicetree alias is not defined"
 #endif
 #endif
 #endif
-
-#define LED_R DT_GPIO_LABEL(LED_NODE_R, gpios)
-#define PIN_R DT_GPIO_PIN(LED_NODE_R, gpios)
-#define FLAGS_R DT_GPIO_FLAGS(LED_NODE_R, gpios)
-
-#define LED_G DT_GPIO_LABEL(LED_NODE_G, gpios)
-#define PIN_G DT_GPIO_PIN(LED_NODE_G, gpios)
-#define FLAGS_G DT_GPIO_FLAGS(LED_NODE_G, gpios)
-
-#define LED_B DT_GPIO_LABEL(LED_NODE_B, gpios)
-#define PIN_B DT_GPIO_PIN(LED_NODE_B, gpios)
-#define FLAGS_B DT_GPIO_FLAGS(LED_NODE_B, gpios)
-
-
-typedef struct cyber60_led {
-    const char *name;
-    gpio_pin_t pin;
-    gpio_flags_t flags;
-} cyber60_led_t;
-
-
-static const cyber60_led_t leds[] = {
-    {LED_R, PIN_R, FLAGS_R},
-    {LED_G, PIN_G, FLAGS_G},
-    {LED_B, PIN_B, FLAGS_B}
-};
 
 #define RED 0
 #define GREEN 1
 #define BLUE 2
 
-static const size_t led_size = sizeof(leds)/sizeof(leds[0]);
+static const struct gpio_dt_spec LED_R = GPIO_DT_SPEC_GET(LED_NODE_R, gpios);
+static const struct gpio_dt_spec LED_G = GPIO_DT_SPEC_GET(LED_NODE_G, gpios);
+static const struct gpio_dt_spec LED_B = GPIO_DT_SPEC_GET(LED_NODE_B, gpios);
 
-void reset_leds()
-{
-    for (int index = 0; index < led_size; index++) {
-        const struct device *dev = device_get_binding(leds[index].name);
-        if (dev == NULL) {
+void reset_leds() {
+    if (!device_is_ready(LED_R.port)) {
+            return;
+    }
+    else
+        gpio_pin_configure_dt(&LED_R, GPIO_DISCONNECTED);
+    if (!device_is_ready(LED_G.port)) {
+            return;
+    }
+    else
+        gpio_pin_configure_dt(&LED_G, GPIO_DISCONNECTED);
+    if (!device_is_ready(LED_B.port)) {
+            return;
+    }
+    else
+        gpio_pin_configure_dt(&LED_B, GPIO_DISCONNECTED);
+}
+void set_led(size_t index) {
+    if(index == RED){
+        int ret;
+        if (!device_is_ready(LED_R.port)) {
             return;
         }
-        gpio_pin_set(dev, leds[index].pin, false);
+        ret = gpio_pin_configure_dt(&LED_R, GPIO_OUTPUT_LOW);
     }
-}
-
-void set_led(size_t index)
-{
-    const struct device *dev = device_get_binding(leds[index].name);
-    if (dev == NULL) {
-        return;
-    }
-    gpio_pin_set(dev, leds[index].pin, true);
-}
-
-static int pwr_led_init(const struct device *dev) {
-    int ret;
-
-    for (int index=0; index < sizeof(leds)/sizeof(leds[0]); index++) {
-        dev = device_get_binding(leds[index].name);
-        if (dev == NULL) {
-            return -EIO;
+    else if(index == GREEN){
+        int ret;
+        if (!device_is_ready(LED_G.port)) {
+            return;
         }
-
-        ret = gpio_pin_configure(dev, leds[index].pin, GPIO_OUTPUT | leds[index].flags);
-        if (ret < 0) {
-            return -EIO;
-        }
-        // set to false so no constant battery drain, just testing for now
-        gpio_pin_set(dev, leds[index].pin, false);
+        ret = gpio_pin_configure_dt(&LED_G, GPIO_OUTPUT_LOW);
     }
-
-    return 0;
+    else if(index == BLUE){
+        int ret;
+        if (!device_is_ready(LED_B.port)) {
+            return;
+        }
+        ret = gpio_pin_configure_dt(&LED_B, GPIO_OUTPUT_LOW);
+    }
+    return;
 }
 
-void led_work_handler(struct k_work *work) {
-    reset_leds();
-}
+void led_work_handler(struct k_work *work) { reset_leds(); }
 
 K_WORK_DEFINE(led_work, led_work_handler);
 
-void led_expiry_function()
-{
-    k_work_submit(&led_work);
-}
+void led_expiry_function() { k_work_submit(&led_work); }
 
 K_TIMER_DEFINE(led_timer, led_expiry_function, NULL);
 
-int led_listener(const zmk_event_t *eh)
-{
+int led_listener(const zmk_event_t *eh) {
     const struct zmk_ble_active_profile_changed *profile_ev = NULL;
-    // uint8_t index = zmk_ble_active_profile_index();
 
     if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL) {
         return ZMK_EV_EVENT_BUBBLE;
@@ -129,26 +99,26 @@ int led_listener(const zmk_event_t *eh)
     k_timer_stop(&led_timer);
 
     reset_leds();
-    switch(profile_ev->index) {
-        case 0:
-            set_led(RED);
-            break;
-        case 1:
-            set_led(GREEN);
-            break;
-        case 2:
-            set_led(BLUE);
-            break;
-        case 3:
-            set_led(RED);
-            set_led(GREEN);
-            break;
-        case 4:
-            set_led(BLUE);
-            set_led(GREEN);
-            break;
-        default:
-            break;
+    switch (profile_ev->index) {
+    case 0:
+        set_led(RED);
+        break;
+    case 1:
+        set_led(GREEN);
+        break;
+    case 2:
+        set_led(BLUE);
+        break;
+    case 3:
+        set_led(RED);
+        set_led(GREEN);
+        break;
+    case 4:
+        set_led(BLUE);
+        set_led(GREEN);
+        break;
+    default:
+        break;
     }
     k_timer_start(&led_timer, K_SECONDS(3), K_NO_WAIT);
 
@@ -157,7 +127,5 @@ int led_listener(const zmk_event_t *eh)
 
 ZMK_LISTENER(led_output_status, led_listener)
 #if defined(CONFIG_ZMK_BLE)
-    ZMK_SUBSCRIPTION(led_output_status, zmk_ble_active_profile_changed);
+ZMK_SUBSCRIPTION(led_output_status, zmk_ble_active_profile_changed);
 #endif
-
-SYS_INIT(pwr_led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
--- config/boards/arm/cyber60_rev_d/cyber60_buzzer.c
+++ config/boards/arm/cyber60_rev_d/cyber60_buzzer.c
@@ -4,15 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr.h>
-#include <kernel.h>
-#include <device.h>
-#include <devicetree.h>
-#include <drivers/pwm.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/pwm.h>
 
-#include <bluetooth/services/bas.h>
+#include <zephyr/bluetooth/services/bas.h>
 
-#include <logging/log.h>
+#include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/ble.h>
@@ -23,96 +22,83 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if !DT_NODE_HAS_STATUS(BUZZER_NODE, okay)
 #error "Unsupported board: buzzer devicetree alias is not defined"
-#define BUZZ_LABEL ""
-#define BUZZ_CHANNEL 0
-#define BUZZ_FLAGS 0
 #else
-#define BUZZ_LABEL DT_LABEL(DT_PWMS_CTLR(BUZZER_NODE))
-#define BUZZ_CHANNEL DT_PWMS_CHANNEL(BUZZER_NODE)
-#define BUZZ_FLAGS DT_PWMS_FLAGS(BUZZER_NODE)
-#endif
 
-#define PERIOD_MIN     50
-#define PERIOD_MAX     3900
-#define PERIOD_INIT    1500
+#endif
 
 #define BEEP_DURATION  K_MSEC(60)
 
-void _play(const struct device *pwm, uint32_t period)
+static const struct pwm_dt_spec pwm = PWM_DT_SPEC_GET(BUZZER_NODE);
+
+void _play(uint32_t period)
 {
-    pwm_pin_set_usec(pwm, BUZZ_CHANNEL, period, period / 2U, BUZZ_FLAGS);
+	if (!device_is_ready(pwm.dev)) {
+		printk("Error: PWM device %s is not ready\n",
+		       pwm.dev->name);
+		return;
+	}
+    pwm_set_dt(&pwm, period, period / 2U); // attempt at new implementation
     k_sleep(BEEP_DURATION);
-
-    pwm_pin_set_usec(pwm, BUZZ_CHANNEL, 0, 0, BUZZ_FLAGS);
+    pwm_set_dt(&pwm, 0, 0); // attempt at new implementation
     k_sleep(K_MSEC(50));
-
 }
 
-void play_sound_1(const struct device *pwm)
+void play_sound_1()
 {
-    _play(pwm, 1000);
-    _play(pwm, 500);
-    _play(pwm, 250);
-    _play(pwm, 100);
-    _play(pwm, 50);
+    _play(1000000);
+    _play(500000);
+    _play(250000);
+    _play(100000);
+    _play(50000);
 }
 
-void play_sound_2(const struct device *pwm)
+void play_sound_2()
 {
-    _play(pwm, 1500);
-    _play(pwm, 3900);
-    _play(pwm, 1500);
-    _play(pwm, 1500);
+    _play(1500000);
+    _play(3900000);
+    _play(1500000);
+    _play(1500000);
 }
 
-void play_sound_3(const struct device *pwm)
+void play_sound_3()
 {
-    _play(pwm, 1500);
-    _play(pwm, 3900);
+    _play(1500000);
+    _play(3900000);
 }
 
-void play_sound_4(const struct device *pwm)
+void play_sound_4()
 {
-    _play(pwm, 2000);
-    _play(pwm, 3900);
+    _play(2000000);
+    _play(3900000);
 }
 
-void play_sound_5(const struct device *pwm)
+void play_sound_5()
 {
-    _play(pwm, 2500);
-    _play(pwm, 3900);
+    _play(2500000);
+    _play(3900000);               
 }
 
 int buzzer_listener(const zmk_event_t *eh)
 {
-
     const struct zmk_ble_active_profile_changed *profile_ev = NULL;
-    const struct device *pwm;
-
     if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL) {
         return ZMK_EV_EVENT_BUBBLE;
     }
-
-    pwm = device_get_binding(BUZZ_LABEL);
-    if (NULL == pwm) {
-        return ZMK_EV_EVENT_BUBBLE;
-    }
-
     switch(profile_ev->index) {
         case 0:
-            play_sound_1(pwm);
+            play_sound_1();
             break;
         case 1:
-            play_sound_2(pwm);
+            play_sound_2();
             break;
         case 2:
-            play_sound_3(pwm);
+            play_sound_3();
             break;
         case 3:
-            play_sound_4(pwm);
+            play_sound_4();
             break;
         case 4:
-            play_sound_5(pwm);
+            play_sound_5();
             break;
         default:
             break;
