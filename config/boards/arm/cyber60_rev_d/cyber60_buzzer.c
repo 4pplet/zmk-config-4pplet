5b11ff84982c360798b9a11d50ee8a227b9fad7e
4pplet <mail@4pplet.com>
2025-03-03 17:45:16 +0100

    adding studio to zmk-config for rev D

--- config/boards/arm/cyber60_rev_d/cyber60_buzzer.c
+++ config/boards/arm/cyber60_rev_d/cyber60_buzzer.c
@@ -1,20 +1,11 @@
-/* Copyright (c) 2021 Megamind (megamind4089)
- * Copyright (c) 2021 The ZMK Contributors
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/pwm.h>
-
-#include <zephyr/bluetooth/services/bas.h>
-
 #include <zephyr/logging/log.h>
+
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#include <zmk/ble.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/ble_active_profile_changed.h>
 
@@ -22,69 +13,61 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if !DT_NODE_HAS_STATUS(BUZZER_NODE, okay)
 #error "Unsupported board: buzzer devicetree alias is not defined"
-#else
-
 #endif
 
-#define BEEP_DURATION  K_MSEC(60)
+#define BEEP_DURATION K_MSEC(60)
 
 static const struct pwm_dt_spec pwm = PWM_DT_SPEC_GET(BUZZER_NODE);
 
-void _play(uint32_t period)
-{
-	if (!device_is_ready(pwm.dev)) {
-		printk("Error: PWM device %s is not ready\n",
-		       pwm.dev->name);
-		return;
-	}
-    pwm_set_dt(&pwm, period, period / 2U); // attempt at new implementation
-    k_sleep(BEEP_DURATION);
-    pwm_set_dt(&pwm, 0, 0); // attempt at new implementation
-    k_sleep(K_MSEC(50));
+static void play_tone(uint32_t period, k_timeout_t duration) {
+    if (!device_is_ready(pwm.dev)) {
+        LOG_ERR("PWM device %s is not ready", pwm.dev->name);
+        return;
+    }
+    pwm_set_dt(&pwm, period, period / 2U);
+    k_sleep(duration);
+    pwm_set_dt(&pwm, 0, 0);
 }
 
-void play_sound_1()
-{
-    _play(1000000);
-    _play(500000);
-    _play(250000);
-    _play(100000);
-    _play(50000);
+static void play_sound_sequence(const uint32_t *tones, size_t count) {
+    for (size_t i = 0; i < count; i++) {
+        play_tone(tones[i], BEEP_DURATION);
+        k_sleep(K_MSEC(50));
+    }
 }
 
-void play_sound_2()
-{
-    _play(1500000);
-    _play(3900000);
-    _play(1500000);
-    _play(1500000);
+void play_sound_1() {
+    const uint32_t tones[] = {1000000, 500000, 250000, 100000, 50000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
 }
 
-void play_sound_3()
-{
-    _play(1500000);
-    _play(3900000);
+void play_sound_2() {
+    const uint32_t tones[] = {1500000, 3900000, 1500000, 1500000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
 }
 
-void play_sound_4()
-{
-    _play(2000000);
-    _play(3900000);
+void play_sound_3() {
+    const uint32_t tones[] = {1500000, 3900000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
 }
 
-void play_sound_5()
-{
-    _play(2500000);
-    _play(3900000);               
+void play_sound_4() {
+    const uint32_t tones[] = {2000000, 3900000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
 }
 
-int buzzer_listener(const zmk_event_t *eh)
-{
-    const struct zmk_ble_active_profile_changed *profile_ev = NULL;
-    if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL) {
+void play_sound_5() {
+    const uint32_t tones[] = {2500000, 3900000};
+    play_sound_sequence(tones, ARRAY_SIZE(tones));
+}
+
+int buzzer_listener(const zmk_event_t *eh) {
+    const struct zmk_ble_active_profile_changed *profile_ev = as_zmk_ble_active_profile_changed(eh);
+    if (!profile_ev) {
         return ZMK_EV_EVENT_BUBBLE;
     }
-    switch(profile_ev->index) {
+
+    switch (profile_ev->index) {
         case 0:
             play_sound_1();
             break;
@@ -103,6 +86,7 @@ int buzzer_listener(const zmk_event_t *eh)
         default:
             break;
     }
+
     return ZMK_EV_EVENT_BUBBLE;
 }
 
--- config/boards/arm/cyber60_rev_d/cyber60_led.c
+++ config/boards/arm/cyber60_rev_d/cyber60_led.c
@@ -1,17 +1,9 @@
-/*
- * Copyright (c) 2021 Megamind (megamind4089)
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
-
-#include <zephyr/bluetooth/services/bas.h>
-
 #include <zephyr/logging/log.h>
+
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/ble.h>
@@ -22,12 +14,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #define LED_NODE_G DT_ALIAS(ledgreen)
 #define LED_NODE_B DT_ALIAS(ledblue)
 
-#if !DT_NODE_HAS_STATUS(LED_NODE_R, okay)
-#if !DT_NODE_HAS_STATUS(LED_NODE_G, okay)
-#if !DT_NODE_HAS_STATUS(LED_NODE_B, okay)
+#if !DT_NODE_HAS_STATUS(LED_NODE_R, okay) || !DT_NODE_HAS_STATUS(LED_NODE_G, okay) || !DT_NODE_HAS_STATUS(LED_NODE_B, okay)
 #error "Unsupported board: led devicetree alias is not defined"
-#endif
-#endif
 #endif
 
 #define RED 0
@@ -38,94 +26,64 @@ static const struct gpio_dt_spec LED_R = GPIO_DT_SPEC_GET(LED_NODE_R, gpios);
 static const struct gpio_dt_spec LED_G = GPIO_DT_SPEC_GET(LED_NODE_G, gpios);
 static const struct gpio_dt_spec LED_B = GPIO_DT_SPEC_GET(LED_NODE_B, gpios);
 
+static void configure_gpio(const struct gpio_dt_spec *led, gpio_flags_t flags) {
+    if (device_is_ready(led->port)) {
+        gpio_pin_configure_dt(led, flags);
+    } else {
+        LOG_ERR("LED device %s is not ready", led->port->name);
+    }
+}
+
 void reset_leds() {
-    if (!device_is_ready(LED_R.port)) {
-            return;
-    }
-    else
-        gpio_pin_configure_dt(&LED_R, GPIO_DISCONNECTED);
-    if (!device_is_ready(LED_G.port)) {
-            return;
-    }
-    else
-        gpio_pin_configure_dt(&LED_G, GPIO_DISCONNECTED);
-    if (!device_is_ready(LED_B.port)) {
-            return;
-    }
-    else
-        gpio_pin_configure_dt(&LED_B, GPIO_DISCONNECTED);
-}
-void set_led(size_t index) {
-    if(index == RED){
-        int ret;
-        if (!device_is_ready(LED_R.port)) {
-            return;
-        }
-        ret = gpio_pin_configure_dt(&LED_R, GPIO_OUTPUT_LOW);
-    }
-    else if(index == GREEN){
-        int ret;
-        if (!device_is_ready(LED_G.port)) {
-            return;
-        }
-        ret = gpio_pin_configure_dt(&LED_G, GPIO_OUTPUT_LOW);
-    }
-    else if(index == BLUE){
-        int ret;
-        if (!device_is_ready(LED_B.port)) {
-            return;
-        }
-        ret = gpio_pin_configure_dt(&LED_B, GPIO_OUTPUT_LOW);
-    }
-    return;
+    configure_gpio(&LED_R, GPIO_OUTPUT_HIGH);
+    configure_gpio(&LED_G, GPIO_OUTPUT_HIGH);
+    configure_gpio(&LED_B, GPIO_OUTPUT_HIGH);
 }
 
-void led_work_handler(struct k_work *work) { reset_leds(); }
-
-K_WORK_DEFINE(led_work, led_work_handler);
-
-void led_expiry_function() { k_work_submit(&led_work); }
-
-K_TIMER_DEFINE(led_timer, led_expiry_function, NULL);
+void set_led_rgb(bool r, bool g, bool b) {
+    reset_leds(); // Ensure all LEDs are off before setting the desired color
+    if (r) {
+        configure_gpio(&LED_R, GPIO_OUTPUT_LOW);
+    }
+    if (g) {
+        configure_gpio(&LED_G, GPIO_OUTPUT_LOW);
+    }
+    if (b) {
+        configure_gpio(&LED_B, GPIO_OUTPUT_LOW);
+    }
+}
 
 int led_listener(const zmk_event_t *eh) {
-    const struct zmk_ble_active_profile_changed *profile_ev = NULL;
-
-    if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL) {
+    const struct zmk_ble_active_profile_changed *profile_ev = as_zmk_ble_active_profile_changed(eh);
+    if (!profile_ev) {
         return ZMK_EV_EVENT_BUBBLE;
     }
-    LOG_WRN("Active profile index:%d", profile_ev->index);
 
-    k_timer_stop(&led_timer);
-
-    reset_leds();
     switch (profile_ev->index) {
-    case 0:
-        set_led(RED);
-        break;
-    case 1:
-        set_led(GREEN);
-        break;
-    case 2:
-        set_led(BLUE);
-        break;
-    case 3:
-        set_led(RED);
-        set_led(GREEN);
-        break;
-    case 4:
-        set_led(BLUE);
-        set_led(GREEN);
-        break;
-    default:
-        break;
+        case 0:
+            set_led_rgb(true, false, false); // Red
+            break;
+        case 1:
+            set_led_rgb(false, true, false); // Green
+            break;
+        case 2:
+            set_led_rgb(false, false, true); // Blue
+            break;
+        case 3:
+            set_led_rgb(true, true, false); // Yellow
+            break;
+        case 4:
+            set_led_rgb(true, false, true); // Magenta
+            break;
+        default:
+            reset_leds();
+            break;
     }
-    k_timer_start(&led_timer, K_SECONDS(3), K_NO_WAIT);
 
     return ZMK_EV_EVENT_BUBBLE;
 }
 
 ZMK_LISTENER(led_output_status, led_listener)
 #if defined(CONFIG_ZMK_BLE)
-ZMK_SUBSCRIPTION(led_output_status, zmk_ble_active_profile_changed);
+    ZMK_SUBSCRIPTION(led_output_status, zmk_ble_active_profile_changed);
 #endif
